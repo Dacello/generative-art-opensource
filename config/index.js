f43336f0540217f8f4c81ce6513caaa72a03a26b
Daniel Andrews <daniel@revelry.co>
2021-12-26 15:34:46 -0500

    Refactor to allow for config based, non-random, individual image generation

---
+++ config/index.js
@@ -0,0 +1,19 @@
+const width = 1000
+const height = 1000
+const config = {
+  attributes: [
+    {name: 'race', type: 'human'},
+    {name: 'class', type: 'fighter'},
+    {name: 'face', type: 'angry'},
+    {name: 'hair', type: 'black'},
+    {name: 'weapon', type: 'longsword'},
+  ],
+  name: "Dacello",
+  description: "Baydayss"
+}
+
+module.exports = {
+  width,
+  height,
+  config
+}
